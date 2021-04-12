56f44b33508ffb2a771fb1391dbb45384b371098
Pierce Rogg <progg@my.devry.edu>
2021-04-11 21:36:13 -0500

    Added Password protection

--- Source.cpp
+++ Source.cpp
@@ -14,6 +14,36 @@ using namespace std;
 /// Entry point to the application
 int main()
 {
+	string password = "";
+	string appPassword = "root170C";
+	bool success = false;
+
+	for (int i; i = 0; i < 3; i++)
+	{
+		cout << "Enter the application password: ";
+		cin >> password;
+		
+		if (password == appPassword)
+		{
+			success = true;
+			break;
+		}
+		else
+		{
+			cout << "\nEnter. Invalid password. Remember the password is case sensitive." << endl;
+			cout << "Try Again.\n" << endl;
+		}
+	}
+
+	if (success == true)
+		cout << "\nWelcome to the application\n" << endl;
+	else
+	{
+		cout << "\nYour IP address and location has been sent to the FBI.\n";
+		system("pause");
+		return -1;
+	}
+
 	// get input
 	int choice = 0;
 
@@ -21,6 +51,7 @@ int main()
 	{
 		// Show Menu
 		system("cls");
+		cout << "Welcome to The DeVry Student Bank's Loan Calculator\n" << endl;
 		cout << "Menu\n" << endl;
 		cout << "1) Add A Loan" << endl;
 		cout << "2) Delete A Loan" << endl;
@@ -47,11 +78,11 @@ int main()
 			// Add
 			cout << "\nAdding a Loan to the file..." << endl;
 			break;
-		case 3:
+		case 2:
 			// Delete
 			cout << "\nDeleting a loan from the file..." << endl;
 			break;
-		case '3':
+		case 3:
 			// Find
 			cout << "\nSearching for a loan in the file..." << endl;
 			break;
